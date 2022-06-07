60acaa1a30587e672fb3a9f5e3d7dccab9eed3d5
Saurav Burjwal <sauravburjwal@gmail.com>
2022-06-07 15:06:52 +0530

    fixing eslint error

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import {
   GridComponent,
@@ -13,24 +14,22 @@ import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Employees = () => {
-  return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Employees" />
-      <GridComponent
-        dataSource={employeesData}
-        allowPaging
-        allowSorting
-        toolbar={['Search']}
-        width="auto"
-      >
-        <ColumnsDirective>
-          {employeesGrid.map((item, index) => (
-            <ColumnDirective key={index} {...item} />
-          ))}
-        </ColumnsDirective>
-        <Inject services={[Page, Search, Toolbar]} />
-      </GridComponent>
-    </div>
-  );
+  <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="Page" title="Employees" />
+    <GridComponent
+      dataSource={employeesData}
+      allowPaging
+      allowSorting
+      toolbar={['Search']}
+      width="auto"
+    >
+      <ColumnsDirective>
+        {employeesGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item} />
+        ))}
+      </ColumnsDirective>
+      <Inject services={[Page, Search, Toolbar]} />
+    </GridComponent>
+  </div>;
 };
 export default Employees;
